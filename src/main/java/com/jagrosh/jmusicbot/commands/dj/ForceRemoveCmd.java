87a445331d2cb5964ae49712ca058e2bbbc8c8bc
Maoyue <95519633+MagicTeaMC@users.noreply.github.com>
2023-08-25 15:59:45 +0800

    allow forceremove when target server left the channel

--- src/main/java/com/jagrosh/jmusicbot/commands/dj/ForceRemoveCmd.java
+++ src/main/java/com/jagrosh/jmusicbot/commands/dj/ForceRemoveCmd.java
@@ -17,16 +17,13 @@ package com.jagrosh.jmusicbot.commands.dj;
 
 import com.jagrosh.jdautilities.command.CommandEvent;
 import com.jagrosh.jdautilities.commons.utils.FinderUtil;
-import com.jagrosh.jdautilities.menu.OrderedMenu;
 import com.jagrosh.jmusicbot.Bot;
 import com.jagrosh.jmusicbot.audio.AudioHandler;
 import com.jagrosh.jmusicbot.commands.DJCommand;
 import net.dv8tion.jda.api.Permission;
-import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.User;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
 
 /**
  *
@@ -39,7 +36,7 @@ public class ForceRemoveCmd extends DJCommand
         super(bot);
         this.name = "forceremove";
         this.help = "移除所有由該使用者加入的歌曲";
-        this.arguments = "<使用者>";
+        this.arguments = "<user>";
         this.aliases = bot.getConfig().getAliases(this.name);
         this.beListening = false;
         this.bePlaying = true;
@@ -63,57 +60,70 @@ public class ForceRemoveCmd extends DJCommand
         }
 
 
-        User target;
-        List<Member> found = FinderUtil.findMembers(event.getArgs(), event.getGuild());
+        User target = findUser(event.getArgs());
 
-        if(found.isEmpty())
+        if (target == null)
         {
             event.replyError("找不到該使用者!");
             return;
-        }
-        else if(found.size()>1)
-        {
-            OrderedMenu.Builder builder = new OrderedMenu.Builder();
-            for(int i=0; i<found.size() && i<4; i++)
-            {
-                Member member = found.get(i);
-                builder.addChoice("**"+member.getUser().getName()+"**#"+member.getUser().getDiscriminator());
-            }
-
-            builder
-            .setSelection((msg, i) -> removeAllEntries(found.get(i-1).getUser(), event))
-            .setText("找到多個成員:")
-            .setColor(event.getSelfMember().getColor())
-            .useNumbers()
-            .setUsers(event.getAuthor())
-            .useCancelButton(true)
-            .setCancel((msg) -> {})
-            .setEventWaiter(bot.getWaiter())
-            .setTimeout(1, TimeUnit.MINUTES)
-
-            .build().display(event.getChannel());
-
-            return;
-        }
-        else
-        {
-            target = found.get(0).getUser();
         }
 
         removeAllEntries(target, event);
 
     }
 
+    private User findUser(String query)
+    {
+        Matcher userMention = FinderUtil.USER_MENTION.matcher(query);
+        Matcher fullRefMatch = FinderUtil.FULL_USER_REF.matcher(query);
+        Matcher discordIdMatch = FinderUtil.DISCORD_ID.matcher(query);
+        if(userMention.matches() || discordIdMatch.matches())
+        {
+            String stringId;
+            if (userMention.matches())
+            {
+                stringId = query.replaceAll("[^0-9]", "");
+            }
+            else
+            {
+                stringId = query;
+            }
+            long userId;
+            try
+            {
+                userId = Long.parseLong(stringId);
+            }
+            catch (NumberFormatException e)
+            {
+                return null;
+            }
+            return bot.getJDA().retrieveUserById(userId).complete();
+        }
+        else if(fullRefMatch.matches())
+        {
+            String username = fullRefMatch.group(1).toLowerCase() + "#" + fullRefMatch.group(2);
+
+            return bot.getJDA().getUserByTag(username);
+        }
+        return null;
+    }
+
     private void removeAllEntries(User target, CommandEvent event)
     {
         int count = ((AudioHandler) event.getGuild().getAudioManager().getSendingHandler()).getQueue().removeAll(target.getIdLong());
+        String discriminator = target.getDiscriminator();
+
         if (count == 0)
         {
-            event.replyWarning("**"+target.getName()+"** 沒有加入任何歌曲!");
+            event.replyWarning("**" + target.getName() + "** 沒有加入任何歌曲!");
         }
         else
         {
-            event.replySuccess("成功移除 `"+count+"` 首由 **"+target.getName()+"**#"+target.getDiscriminator()+" 新增的歌曲");
+            if ("0000".equals(discriminator)) {
+                event.replySuccess("成功移除 `" + count + "` 首由 **" + target.getName() + " 新增的歌曲");
+            } else {
+                event.replySuccess("成功移除 `" + count + "` 首由 **" + target.getName() + "#" + discriminator + " 新增的歌曲");
+            }
         }
     }
 }
