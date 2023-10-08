d629de7fe7313b653e83daaf7835b03536463074
Maoyue <95519633+MagicTeaMC@users.noreply.github.com>
2023-10-08 22:58:50 +0800

    try to fix lyrics

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
     api("net.dv8tion:JDA:4.4.1_353")
     api("dev.arbjerg:lavaplayer:2.0.2")
     api("com.jagrosh:jda-utilities:3.0.5")
-    api("com.github.MagicTeaMC:MaoLyrics:5fd167f")
+    api("com.github.MagicTeaMC:MaoLyrics:8f6ee88")
     api("ch.qos.logback:logback-classic:1.4.11")
     api("com.typesafe:config:1.4.2")
     api("org.jsoup:jsoup:1.16.1")
