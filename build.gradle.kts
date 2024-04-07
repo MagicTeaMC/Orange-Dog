e7fbaa01c69367548b8d4e77ef1c85c181068f66
Maoyue <95519633+MagicTeaMC@users.noreply.github.com>
2024-04-07 17:18:52 +0800

    use other lavaplayer PR to fix some issues

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,7 +26,7 @@ repositories {
 
 dependencies {
     api("net.dv8tion:JDA:5.0.0-beta.21")
-    api("com.github.kamilake:lavaplayer:main-SNAPSHOT")
+    api("com.github.Nansess:lavaplayer:main-SNAPSHOT")
     api("pw.chew:jda-chewtils:2.0-SNAPSHOT")
     api("com.github.MagicTeaMC:MaoLyrics:b74346f")
     api("ch.qos.logback:logback-classic:1.5.3")
